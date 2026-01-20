4c07e7f0a73fe47b41ea9e38ffea331827639dd2
fadil-h25 <fadilhambali25@gmail.com>
2026-01-20 19:14:04 +0800

    feat(pegawai): menambahkan logic dasar untuk manajemen pegawai

--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -2,20 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pegawai extends Model
 {
+    use HasFactory;
+
+    protected $table = 'pegawais';
+
     protected $fillable = [
+        // Identitas
         'nip_baru',
         'nip_lama',
         'nama',
         'gelar_depan',
         'gelar_blk',
-        'keterangan',
         'tempat_lahir_nama',
-        'tgl_lahir',
         'jenis_kelamin',
+        'gol_darah',
         'agama_nama',
         'jenis_kawin_nama',
         'nik',
@@ -23,6 +28,10 @@ class Pegawai extends Model
         'email',
         'email_gov',
         'alamat',
+        'tgl_lahir',
+        'usia',
+
+        // Administrasi
         'npwp_nomor',
         'bpjs',
         'kartu_pegawai',
@@ -32,38 +41,65 @@ class Pegawai extends Model
         'nomor_sk_pns',
         'tgl_sk_pns',
         'tmt_pns',
+        'no_sk_dpk_penugasan_kontrak',
+        'tgl_sk_dpk_penugasan_kontrak',
+        'keterangan',
+        'keterangan_status',
+
+        // Golongan & Jabatan
         'gol_awal_nama',
         'gol_nama',
         'tmt_golongan',
+        'mkgol',
         'jenis_jabatan_nama',
         'jabatan_nama',
         'tmt_jabatan',
+        'jabatan_non_definitif',
+        'jabatan_non_definitif_1',
+        'mkjab',
+        'jumlah',
+        'kelas',
+        'kelas_jabatan',
+        'kelompok_jabatan',
+        'nm_kelompok_jabatan',
+        'nama_kelompok_jabatan',
+        'pangkat',
+        'proyeksi_jf',
+
+        // Pendidikan
+        'tingkat_pendidikan_nama',
+        'pendidikan_nama',
+        'tahun_lulus',
+        'riwayat_diklatpim',
+
+        // Unit & Organisasi
+        'satuan_kerja',
+        'unit_kerja',
+        'unit_organisasi',
         'unor_nama',
         'instansi_induk_nama',
         'eselon',
-        'kelompok_jabatan',
-        'nm_kelompok_jabatan',
+        'divisi',
+        'ukm',
         'range_umur',
-        'kelas_jabatan',
-        'keterangan_status',
-        'tingkat_pendidikan_nama',
-        'satuan_kerja',
+
+        // Lokasi
         'provinsi',
         'kab_kota',
+
+        // Status Pegawai
         'jenis_pegawai',
         'status_kepegwaian',
     ];
 
-    protected function casts(): array
-    {
-        return [
-            'tgl_lahir' => 'date',
-            'tgl_sk_cpns' => 'date',
-            'tmt_cpns' => 'date',
-            'tgl_sk_pns' => 'date',
-            'tmt_pns' => 'date',
-            'tmt_golongan' => 'date',
-            'tmt_jabatan' => 'date',
-        ];
-    }
+    protected $casts = [
+        'tgl_lahir' => 'date',
+        'tgl_sk_cpns' => 'date',
+        'tmt_cpns' => 'date',
+        'tgl_sk_pns' => 'date',
+        'tmt_pns' => 'date',
+        'tgl_sk_dpk_penugasan_kontrak' => 'date',
+        'tmt_golongan' => 'date',
+        'tmt_jabatan' => 'date',
+    ];
 }
