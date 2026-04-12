c9a1c3f5af2b1fe5250213afbcae68238577b1a5
Fadil Hambali <fadilhambali25@gmail.com>
2026-04-13 01:58:39 +0800

    refactor: menghilangkan sisa sisa kodingan folio

--- resources/views/pages/admin/kgbs/import.blade.php
+++ resources/views/pages/admin/kgbs/import.blade.php
@@ -1,10 +1,3 @@
-@php
-use function Laravel\Folio\{middleware, name};
-
-name('admin.kgbs.import');
-middleware(['auth', 'verified']);
-@endphp
-
 <x-layouts.app title="Import Data KGB">
     <livewire:admin.kgbs.import />
 </x-layouts.app>
--- resources/views/pages/admin/kgbs/index.blade.php
+++ resources/views/pages/admin/kgbs/index.blade.php
@@ -1,10 +1,3 @@
-@php
-use function Laravel\Folio\{middleware, name};
-
-name('admin.kgbs');
-middleware(['auth', 'verified']);
-@endphp
-
 <x-layouts.app title="Kenaikan Gaji Berkala">
     <livewire:admin.kgbs.index />
 </x-layouts.app>
