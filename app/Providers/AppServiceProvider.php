65e13f1c10048abc5aa979211831564c0a8eae2e
Fachrul <fachrul.nawawi@gmail.com>
2026-05-18 11:21:30 +0800

    Perbaikan chart agar otomatis bekerja saat perubahan alamat IP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (!app()->runningInConsole()) {
+            $url = request()->root();
+            config(['app.url' => $url]);
+            \Illuminate\Support\Facades\URL::forceRootUrl($url);
+
+            // Fix for Livewire in subdirectory
+            $path = request()->getBasePath();
+            if ($path) {
+                config(['livewire.asset_url' => $url]);
+                \Livewire\Livewire::setUpdateRoute(function ($handle) use ($path) {
+                    return \Illuminate\Support\Facades\Route::post($path.'/livewire/update', $handle);
+                });
+            }
+        }
+
         Livewire::component('livewire-pie-chart', LivewirePieChart::class);
         Livewire::component('livewire-column-chart', LivewireColumnChart::class);
     }
